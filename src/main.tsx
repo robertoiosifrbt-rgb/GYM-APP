d5e76c76114b947a3681ab91f97566a21b32beb8
robertoiosifrbt-rgb <robertoiosifrbt@gmail.com>
2026-08-12 10:30:34 +0100

    Load visual target shell overrides

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './redesign.css'
+import './target-shell.css'
 import App from './app/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
